43d49e725be8613cac78ff037975be662948d560
nuv <git@nuv.direct>
2025-08-31 19:37:50 +0200

    swap @all to @inputs

--- flake.nix
+++ flake.nix
@@ -5,9 +5,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = all@{ self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     # Used with `nixos-rebuild --flake .#<hostname>`
     # nixosConfigurations."<hostname>".config.system.build.toplevel must be a derivation
+
+    # shitbox config
     nixosConfigurations.shitbox = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ 
@@ -15,5 +17,14 @@
         ./hardware-configuration-shitbox.nix
       ];
     };
+
+    # some other device
+    #nixosConfigurations.other-device = nixpkgs.lib.nixosSystem {
+    #  system = "x86_64-linux";
+    #  modules = [ 
+    #    ./configuration.nix 
+    #    ./hardware-configuration-other-device.nix
+    #  ];
+    #};
   };
 }
